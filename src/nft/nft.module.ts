0b0364af45c2792ffc1a569ee8d3edf395f11a9e
optimus789 <vinaysudrik@gmail.com>
2022-09-26 20:05:29 +0530

    fixed issue with httpmodule

--- src/nft/nft.module.ts
+++ src/nft/nft.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { NftController } from './nft.controller';
 import { NftService } from './nft.service';
@@ -5,5 +6,6 @@ import { NftService } from './nft.service';
 @Module({
   controllers: [NftController],
   providers: [NftService],
+  imports: [HttpModule],
 })
 export class NftModule {}
