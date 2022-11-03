e29ca88d56a610ac1480d7af8dd8741d17af916a
SwaroopLightning <sathyaswaroopvandavasi@gmail.com>
2022-11-03 17:20:31 +0530

    added get assets and get sigle asset routes

--- src/nft/nft.service.ts
+++ src/nft/nft.service.ts
@@ -4,7 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ContractDocument, ContractSchema } from 'src/schemas/contract.schema';
 import { NftDocument, NftSchema } from 'src/schemas/nft.schema';
-import { createNFT, getNft } from './nftitems/createNft.dto';
+import { createNFT, getNft, get_Nft_body } from './nftitems/createNft.dto';
 
 @Injectable()
 export class NftService {
@@ -34,10 +34,24 @@ export class NftService {
   async createNFT(data: any): Promise<any> {
     return await (await this.NftModel.create(data)).save();
   }
-  async getallnfts() {
-    return await this.NftModel.find({});
+  // To get all Nfts
+  async GetAllNfts(): Promise<any> {
+    try {
+      return await this.NftModel.find({});
+    } catch (error) {
+      console.log(error);
+      return { message: 'Something went wrong' };
+    }
   }
-
+  // To get single Nft
+  async GetNft(data: get_Nft_body): Promise<any> {
+    try {
+      return await this.NftModel.findOne(data);
+    } catch (error) {
+      console.log(error);
+      return { message: 'Something went Wrong' };
+    }
+  }
   async getcollections() {
     return await this.ContractModel.find({});
   }
