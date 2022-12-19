52d6bcbfb6ff8eca2f835ecded81e0e9cfc2e518
Sathya Swaroop Vandavasi <sathyaswaroopvandavasi@gmail.com>
2022-12-19 20:36:49 +0530

    added examples for activity

--- src/activity/dtos/itemdto/item-activity.dto.ts
+++ src/activity/dtos/itemdto/item-activity.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class GetItemActivity {
-  @ApiProperty()
+  @ApiProperty({ example: "0xc5195CDa9ED7dC18AFA7b69Da90Bbaf427C1ca3F" })
   contract_address: string;
-  @ApiProperty()
+  @ApiProperty({ example: "1" })
   token_id: string;
-  @ApiProperty()
+  @ApiProperty({ example: 5 })
   page_number: number;
-  @ApiProperty()
+  @ApiProperty({ example: 5 })
   items_per_page: number;
 }
