decafc42a9c6cffb04b02b6b5f83b6fc6183f685
Sathya Swaroop Vandavasi <sathyaswaroopvandavasi@gmail.com>
2022-12-23 13:42:22 +0530

    Added get Collections Owned API

--- src/nft/nftitems/collections.dto.ts
+++ src/nft/nftitems/collections.dto.ts
@@ -5,3 +5,11 @@ export class GetCollectionBody {
     @ApiProperty({ default: 5 })
     items_per_page: number;
 }
+export class GetUserOwnedCollections {
+    @ApiProperty({ default: '0xa8E7CCE298F1C2e52DE6920840d80C28Fc787F72' })
+    owner_address: string;
+    @ApiProperty({ default: 1, minimum: 1 })
+    page_number: number;
+    @ApiProperty({ default: 5 })
+    items_per_page: number;
+}
